2ed063b781a681c8289a5ab5389c045bf43cd385
DanielaVenturi <danielaventuri14@gmail.com>
2024-07-24 15:27:28 -0300

    criação da const nome_maiusculo em Marca

--- core/models/marca.py
+++ core/models/marca.py
@@ -3,10 +3,17 @@ from django.db import models
 class Marca(models.Model):
     nome = models.CharField(max_length=50)
     nacionalidade = models.CharField(max_length=50, blank=True, null=True)
+    
     def __str__(self):
-       return f"{self.nome.upper()} ({self.nacionalidade}) ({self.id})"
+        
+        nome_maiusculo = self.nome.upper()
+        
+       
+        if self.nacionalidade:
+            return f"{nome_maiusculo} ({self.nacionalidade}) ({self.id})"
+        else:
+            return f"{nome_maiusculo} ({self.id})"
+    
     class Meta:
-        """Meta options for the model."""
-
         verbose_name = "Marca"
         verbose_name_plural = "Marcas"
